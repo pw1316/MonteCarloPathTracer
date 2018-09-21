200ee42da45258edfdd82d343dd46a288831620c
Joker Yough <of19950402@gmail.com>
2018-09-21 16:25:42 +0800

    dx11

--- MCRT/QuinEngine/DX11/GraphicsDX11.hpp
+++ MCRT/QuinEngine/DX11/GraphicsDX11.hpp
@@ -8,123 +8,198 @@
 #include <D3D11.h>
 #include <D3DX10math.h>
 #pragma comment(lib, "d3d11.lib")
+#if _DEBUG
+#pragma comment(lib, "d3dx10d.lib")
+#else
 #pragma comment(lib, "d3dx10.lib")
+#endif
 #pragma warning(pop)
 
-#include <DX11/ModelDX11.hpp>
-#include <DX11/Font.hpp>
+#include <tiny_obj_loader.h>
+#if _DEBUG
+#pragma comment(lib, "tinyobjloaderd.lib")
+#else
+#pragma comment(lib, "tinyobjloader.lib")
+#endif
 
 namespace Quin::System::DX11
 {
-    struct Camera
-    {
-        Camera() :m_x(0), m_y(0), m_z(0), m_rotx(0), m_roty(0), m_rotz(0) {}
-        void Get(D3DXMATRIX& m)
-        {
-            if (m_dirty)
-            {
-                D3DXVECTOR3 pos(m_x, m_y, m_z), up(0.0f, 1.0f, 0.0f), lookAt(0.0f, 0.0f, 1.0f);
-                D3DXMATRIX rotMatrix{};
-                D3DXMatrixRotationYawPitchRoll(&rotMatrix, m_roty, m_rotx, m_rotz);
-                D3DXVec3TransformCoord(&lookAt, &lookAt, &rotMatrix);
-                D3DXVec3TransformCoord(&up, &up, &rotMatrix);
-                lookAt = pos + lookAt;
-                D3DXMatrixLookAtLH(&m_matrix, &pos, &lookAt, &up);
-                m_dirty = false;
-            }
-            m = m_matrix;
-        }
-        D3DXVECTOR4 Pos()
-        {
-            return D3DXVECTOR4(m_x, m_y, m_z, 1.0f);
-        }
-        BOOL m_dirty = true;
-        union
-        {
-            D3DXVECTOR3 pos;
-            struct
-            {
-                FLOAT m_x, m_y, m_z;
-            };
-        };
-        union
-        {
-            D3DXVECTOR3 rot;
-            struct
-            {
-                FLOAT m_rotx, m_roty, m_rotz;
-            };
-        };
-        D3DXMATRIX m_matrix;
-    };
-
-    struct Light
-    {
-        D3DXVECTOR3 m_dir;
-    };
-
     class GraphicsDX11 : public Core::Graphics
     {
     public:
-        GraphicsDX11() = default;
-        virtual ~GraphicsDX11() = default;
+        void Initialize(HWND hWnd, UINT w, UINT h) override
+        {
+            HRESULT hr = S_OK;
 
-        /* Override */
-        virtual void Initialize(HWND hwnd, UINT w, UINT h);
-        virtual void Shutdown();
-        virtual BOOL OnUpdate();
-        virtual LRESULT CALLBACK MessageHandler(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+            ID3D11Texture2D *texture2D = nullptr;
+
+            D3D11_TEXTURE2D_DESC texture2DDesc;
+            DXGI_SWAP_CHAIN_DESC SCDesc;
+            D3D11_RASTERIZER_DESC RSDesc;
+            D3D11_DEPTH_STENCIL_DESC DSDesc;
+            D3D11_DEPTH_STENCIL_VIEW_DESC DSVDesc;
+            D3D11_VIEWPORT viewport;
+
+            m_w = w;
+            m_h = h;
+
+            ZeroMemory(&SCDesc, sizeof(SCDesc));
+            SCDesc.BufferDesc.Width = w;
+            SCDesc.BufferDesc.Height = h;
+            SCDesc.BufferDesc.RefreshRate.Numerator = 60;// 60FPS
+            SCDesc.BufferDesc.RefreshRate.Denominator = 1;// 60FPS
+            SCDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+            SCDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+            SCDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+            SCDesc.SampleDesc.Count = 1;// MSAA off
+            SCDesc.SampleDesc.Quality = 0;// MSAA off
+            SCDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+            SCDesc.BufferCount = 1;// Single Buffer
+            SCDesc.OutputWindow = hWnd;
+            SCDesc.Windowed = true;
+            SCDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+            SCDesc.Flags = 0;// No Advanced Flags
+            D3D_FEATURE_LEVEL featureLevel = D3D_FEATURE_LEVEL_11_0;
+            hr = D3D11CreateDeviceAndSwapChain(
+                nullptr,// Adapter
+                D3D_DRIVER_TYPE_HARDWARE,
+                nullptr,
+                D3D11_CREATE_DEVICE_DEBUG,
+                &featureLevel, 1,
+                D3D11_SDK_VERSION,
+                &SCDesc, &m_swapchain,
+                &m_device,
+                nullptr,
+                &m_context
+            );
+            FAILTHROW;
+
+            /* IA */
+
+            /* VS */
+
+            /* Rasterizer */
+            ZeroMemory(&RSDesc, sizeof(RSDesc));
+            RSDesc.FillMode = D3D11_FILL_SOLID;
+            RSDesc.CullMode = D3D11_CULL_BACK;
+            RSDesc.FrontCounterClockwise = false;
+            RSDesc.DepthBias = 0;
+            RSDesc.DepthBiasClamp = 0.0f;
+            RSDesc.SlopeScaledDepthBias = 0.0f;
+            RSDesc.DepthClipEnable = true;
+            RSDesc.ScissorEnable = false;
+            RSDesc.MultisampleEnable = false;
+            RSDesc.AntialiasedLineEnable = false;
+            hr = m_device->CreateRasterizerState(&RSDesc, &m_rasterizerState);
+            FAILTHROW;
+            ZeroMemory(&viewport, sizeof(viewport));
+            viewport.TopLeftX = 0.0f;
+            viewport.TopLeftY = 0.0f;
+            viewport.Width = static_cast<FLOAT>(w);
+            viewport.Height = static_cast<FLOAT>(h);
+            viewport.MinDepth = 0.0f;
+            viewport.MaxDepth = 1.0f;
+
+            /* PS */
+
+            /* OM */
+            hr = m_swapchain->GetBuffer(0, IID_PPV_ARGS(&texture2D));
+            FAILTHROW;
+            hr = m_device->CreateRenderTargetView(texture2D, nullptr, &m_RTV);
+            FAILTHROW;
+            SafeRelease(&texture2D);
+            ZeroMemory(&texture2DDesc, sizeof(texture2DDesc));
+            texture2DDesc.Width = w;
+            texture2DDesc.Height = h;
+            texture2DDesc.MipLevels = 1;
+            texture2DDesc.ArraySize = 1;
+            texture2DDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+            texture2DDesc.SampleDesc.Count = 1;
+            texture2DDesc.SampleDesc.Quality = 0;
+            texture2DDesc.Usage = D3D11_USAGE_DEFAULT;
+            texture2DDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+            texture2DDesc.CPUAccessFlags = 0;
+            texture2DDesc.MiscFlags = 0;
+            hr = m_device->CreateTexture2D(&texture2DDesc, NULL, &texture2D);
+            FAILTHROW;
+            ZeroMemory(&DSVDesc, sizeof(DSVDesc));
+            DSVDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+            DSVDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+            DSVDesc.Texture2D.MipSlice = 0;
+            hr = m_device->CreateDepthStencilView(texture2D, &DSVDesc, &m_DSV);
+            FAILTHROW;
+            SafeRelease(&texture2D);
+            ZeroMemory(&DSDesc, sizeof(DSDesc));
+            DSDesc.DepthEnable = true;
+            DSDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
+            DSDesc.DepthFunc = D3D11_COMPARISON_LESS;
+            DSDesc.StencilEnable = false;
+            DSDesc.StencilReadMask = 0xFF;
+            DSDesc.StencilWriteMask = 0xFF;
+            DSDesc.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+            DSDesc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_INCR;
+            DSDesc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+            DSDesc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+            DSDesc.BackFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+            DSDesc.BackFace.StencilDepthFailOp = D3D11_STENCIL_OP_DECR;
+            DSDesc.BackFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+            DSDesc.BackFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+            hr = m_device->CreateDepthStencilState(&DSDesc, &m_DSS);
+            FAILTHROW;
+        }
+        void Shutdown() override
+        {
+            m_context->OMSetRenderTargets(0, nullptr, nullptr);
+            m_context->OMSetDepthStencilState(nullptr, 1);
+
+            m_context->RSSetViewports(0, nullptr);
+            m_context->RSSetState(nullptr);
+
+            SafeRelease(&m_DSS);
+            SafeRelease(&m_DSV);
+            SafeRelease(&m_RTV);
+
+            SafeRelease(&m_rasterizerState);
+
+            SafeRelease(&m_context);
+            SafeRelease(&m_device);
+            SafeRelease(&m_swapchain);
+        }
+        BOOL OnUpdate() override
+        {
+            BeginScene();
+            EndScene();
+            return true;
+        }
+        LRESULT CALLBACK MessageHandler(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) override
+        {
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
     private:
-        /* D3D Basic */
-        void InitializeDevice(HWND hwnd);
-        void ShutdownDevice();
-
-        /* Output Merger */
-        void InitializeOM();
-        void ShutdownOM();
-
-        /* Output Merger */
-        void InitializeRasterizer();
-        void ShutdownRasterizer();
-
         void BeginScene()
         {
-            float color[] = { 0.25f, 0.1f, 0.15f, 0.0f };
+            FLOAT color[] = { 0.2f, 0.15f, 0.15f, 0.0f };
             m_context->ClearRenderTargetView(m_RTV, color);
             m_context->ClearDepthStencilView(m_DSV, D3D11_CLEAR_DEPTH, 1.0f, 0);
         }
         void EndScene()
         {
-            m_swapChain->Present(1, 0);
+            m_swapchain->Present(1, 0);
         }
-        void OnRender();
-        void OnGUI();
+        UINT m_w = 640U;
+        UINT m_h = 480U;
 
-        /* D3D Basic */
-        UINT m_width = 640;
-        UINT m_height = 480;
-        IDXGISwapChain *m_swapChain = nullptr;
+        /* D3D Handle */
+        IDXGISwapChain *m_swapchain = nullptr;
         ID3D11Device *m_device = nullptr;
         ID3D11DeviceContext *m_context = nullptr;
 
-        /* OM Dynamic */
+        /* Rasterizer */
+        ID3D11RasterizerState *m_rasterizerState = nullptr;
+
+        /* OM */
         ID3D11RenderTargetView *m_RTV = nullptr;
         ID3D11DepthStencilView *m_DSV = nullptr;
-        ID3D11DepthStencilState *m_DSSWithZ = nullptr;
-        ID3D11DepthStencilState *m_DSSWithoutZ = nullptr;
-        ID3D11BlendState *m_BSWithBlend = nullptr;
-        ID3D11BlendState *m_BSWithoutBlend = nullptr;
-
-        /* RS Dynamic */
-        // No Components Will Change For Now
-
-        /* Projection Matrix */
-        D3DXMATRIX m_MatrixProj{};
-        D3DXMATRIX m_MatrixOrtho{};
-
-        Camera m_camera;
-        ModelDX11 *m_model = nullptr;
-        Font *m_gui = nullptr;
-        Light m_light;
+        ID3D11DepthStencilState *m_DSS = nullptr;
     };
 }
